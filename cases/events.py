0b6cf6cd8dfec52aff197b49f56e382747782a7b
M. Dietrich <mdt@emdete.de>
2022-01-07 22:06:29 +0100

    fix video

--- cases/events.py
+++ cases/events.py
@@ -10,32 +10,32 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 class Case(object):
-    def __init__(self, std):
-        pass
+	def __init__(self, std):
+		pass
 
-    def start(self):
-        yield 'Plug & unplug the headphone'
-        toggles = 0
-        infile_path = '/dev/input/event14'
-        '''
-        FORMAT represents the format used by linux kernel input event struct
-        See https://github.com/torvalds/linux/blob/v5.5-rc5/include/uapi/linux/input.h#L28
-        Stands for: long int, long int, unsigned short, unsigned short, unsigned int
-        '''
-        FORMAT = 'llHHI'
-        EVENT_SIZE = struct.calcsize(FORMAT)
-        with open(infile_path, 'rb') as in_file:
-            event = in_file.read(EVENT_SIZE)
-            while event:
-                (tv_sec, tv_usec, type_, code, value) = struct.unpack(FORMAT, event)
-                if type_ != 0 or code != 0 or value != 0:
-                    toggles += 1
-                    yield 'Event type {}, code {}, value {} at {}.{}'.format(
-                        type_, code, value, tv_sec, tv_usec)
-                else:
-                    # Events with code, type and value == 0 are 'separator' events
-                    pass
-                if toggles >= 2:
-                    return
-                event = in_file.read(EVENT_SIZE)
+	def start(self):
+		yield 'Plug & unplug the headphone'
+		toggles = 0
+		infile_path = '/dev/input/event14'
+		'''
+		FORMAT represents the format used by linux kernel input event struct
+		See https://github.com/torvalds/linux/blob/v5.5-rc5/include/uapi/linux/input.h#L28
+		Stands for: long int, long int, unsigned short, unsigned short, unsigned int
+		'''
+		FORMAT = 'llHHI'
+		EVENT_SIZE = struct.calcsize(FORMAT)
+		with open(infile_path, 'rb') as in_file:
+			event = in_file.read(EVENT_SIZE)
+			while event:
+				(tv_sec, tv_usec, type_, code, value) = struct.unpack(FORMAT, event)
+				if type_ != 0 or code != 0 or value != 0:
+					toggles += 1
+					yield 'Event type {}, code {}, value {} at {}.{}'.format(
+						type_, code, value, tv_sec, tv_usec)
+				else:
+					# Events with code, type and value == 0 are 'separator' events
+					pass
+				if toggles >= 2:
+					return
+				event = in_file.read(EVENT_SIZE)
 
--- cases/video.py
+++ cases/video.py
@@ -13,23 +13,22 @@ class Case(object):
 		pass
 
 	def start(self):
-		return
 		for filename in scandir('/dev'):
 			if filename.name.startswith('video'):
 				video = v4l2capture.Video_device(filename.path)
 				try:
 					driver, card, bus_info, capabilities = video.get_info()
 					capabilities = ', '.join([n.decode() for n in capabilities])
-					print(f'driver={driver}; card={card}bus; info={bus_info}; capabilities={capabilities}.')
+					yield f'{filename.name}; driver={driver}; card={card}bus; info={bus_info}; capabilities={capabilities}'
 					try:
-						print('format', video.get_format())
+						yield '\tformat {}'.format(video.get_format())
 						for fourcc in video.get_formats():
 							for framesize in video.get_framesizes(fourcc):
 								x, y = framesize['size_x'], framesize['size_y']
 								for frameinterval in video.get_frameintervals(fourcc, x, y):
-									yield '\t', fourcc.decode(), x, y, frameinterval['fps']
+									yield '\t\t{} {} {} {}'.format(fourcc.decode(), x, y, frameinterval['fps'])
 					except:
-						pass
+						yield 'error'
 				finally:
 					video.close()
 
