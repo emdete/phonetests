919b34fef023bbbc810a43c07da3eacf4b90e35f
M. Dietrich <mdt@emdete.de>
2022-01-13 16:19:12 +0100

    simplify thermal

--- go/thermal.go
+++ go/thermal.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"math"
 	"strings"
+	"strconv"
 	"io/fs"
 	"path/filepath"
 	)
@@ -10,6 +12,9 @@ import (
 // func readFile(path string) (string, error) { from rfkill.go
 
 func thermal() {
+	temp := 0.0
+	type_ := ""
+	type2 := ""
 	thermalpath := "/sys/devices/virtual/thermal"
 	if err := filepath.Walk(thermalpath, func(path string, info fs.FileInfo, err error) error {
 		if thermalpath == path {
@@ -22,15 +27,27 @@ func thermal() {
 			for _,filename := range []string{"type", "temp"} {
 				value := "undefined"
 				if val, err := readFile(filepath.Join(path, filename)); err != nil {
-					//return err
+					return err
 				} else {
 					value = val
 				}
-					log.Printf("%s %s=%s\n", path, filename, value)
+				if filename == "temp" {
+					if tf, err := strconv.ParseInt(value, 10, 64); err == nil {
+						temp2 := math.Max(temp, float64(tf) / 1000.0)
+						if temp2 != temp {
+							temp = temp2
+							type_ = type2
+						}
+					}
+				} else {
+					type2 = value
+				}
+				log.Printf("%s %s=%s\n", path, filename, value)
 			}
 		}
 		return nil
 	}); err != nil {
 		panic(err)
 	}
+	log.Printf("temp max=%f from %s", temp, type_)
 }
